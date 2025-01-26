f280f923c944a2e5011b865fd0e2407f64e837e6
skytech2510 <skytech2510@gmail.com>
2025-01-27 05:15:44 +1000

    feat: upload image to pinata

--- app/NFT/page.tsx
+++ app/NFT/page.tsx
@@ -1,8 +1,25 @@
+"use client"
+import { useEffect, useState } from "react";
+import { uploadImage } from "../utiles";
+
 const NFT: React.FC = () => {
+    const [file, setFile] = useState<File | null>(null);
+
+    const handleMint = async () => {
+        console.log(await uploadImage(file));
+    }
     return (
-        <>
-            asdfsdf
-        </>
+        <div>
+            <div>
+                <input type="file" onChange={(e) => {
+                    setFile(e.target.files[0]);
+                }} />
+                <input type="text" />
+            </div>
+            <div>
+                <button onClick={handleMint}>Mint</button>
+            </div>
+        </div>
     )
 }
 export default NFT;
---
+++ app/utiles.tsx
@@ -0,0 +1,21 @@
+import axios from "axios";
+const uploadImage = async (file: File) => {
+    if (!file) return;
+    const formData = new FormData();
+    formData.append("file", file);
+    try {
+        const response = await axios.post('https://api.pinata.cloud/pinning/pinFileToIPFS', formData, {
+            headers: {
+                'Content-Type': "multipart/form-data",
+                'pinata_api_key': "f405d86a2838de081d08",
+                'pinata_secret_api_key': "c223f69a7ed8cd82b5a94d432387563f2d7536915eefdb9e1c24be578b4a6b26"
+
+            }
+        })
+        return `https://ipfs.io/ipfs/${response.data.IpfsHash}`;
+    }
+    catch (e) {
+        console.log(e)
+    }
+}
+export { uploadImage };
