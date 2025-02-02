fefb90931213d351b0ec623bb05732d64563b6eb
skytech2510 <skytech2510@gmail.com>
2025-02-02 23:32:48 +1000

    update

---
+++ app/types.ts
@@ -0,0 +1,10 @@
+interface metadataTypes {
+    name: string,
+    symbol: string,
+    uri: string,
+    seller_fee_basis_points: number,
+    creator: string,
+}
+export type {
+    metadataTypes
+}
--- app/utiles.tsx
+++ app/utiles.tsx
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { metadataTypes } from "./types";
 const uploadImage = async (file: File) => {
     if (!file) return;
     const formData = new FormData();
@@ -15,7 +16,23 @@ const uploadImage = async (file: File) => {
         return `https://ipfs.io/ipfs/${response.data.IpfsHash}`;
     }
     catch (e) {
-        console.log(e)
+        console.log(e);
+        return e;
     }
 }
-export { uploadImage };
+const uploadMetadata = async (metadata: metadataTypes) => {
+    try {
+        const response = await axios.post('https://api.pinata.cloud/pinning/pinJSONToIPFS', metadata, {
+            headers: {
+                "Content-Type": "application/json",
+                'pinata_api_key': "f405d86a2838de081d08",
+                'pinata_secret_api_key': "c223f69a7ed8cd82b5a94d432387563f2d7536915eefdb9e1c24be578b4a6b26"
+            }
+        })
+        return `https://ipfs.io/ipfs/${response.data.IpfsHash}`;
+    }
+    catch (e) {
+        return e;
+    }
+}
+export { uploadImage, uploadMetadata };
